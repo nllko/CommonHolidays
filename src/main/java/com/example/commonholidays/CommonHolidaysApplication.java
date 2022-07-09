0f09e8623c2d82f8c73c72896783dcc81b342234
f0rsakenOne <nikolaj.aleksejenkov@gmail.com>
2022-07-09 23:48:42 +0300

    initialization

---
+++ src/main/java/com/example/commonholidays/CommonHolidaysApplication.java
@@ -0,0 +1,13 @@
+package com.example.commonholidays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonHolidaysApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CommonHolidaysApplication.class, args);
+  }
+
+}
